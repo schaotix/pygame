7278dc6246dfb5fa8f86d10d988dcb3071bbb8ce
schaotix <screamer@hackermind.net>
2022-12-13 20:22:33 -0500

    Updated movement.

--- space.py
+++ space.py
@@ -15,21 +15,20 @@ def main():
 	ship = pygame.image.load("ship.png")
 	rock = pygame.image.load("rock.png")
 	bg_img = pygame.image.load("space.jpg")
-	
+
 	rock_loc = rock.get_rect()
 	# rock_loc.center = width / 2, height * -1
 	first_col = width / 4 + random.randint(0,300)
 	second_col = width / 4 + random.randint(301,600)
 	third_col = width / 4 + random.randint(601,900)
 	fourth_col = width / 4 + random.randint(901,1200)
-	
+
 	ship_loc = ship.get_rect()
 	ship_loc.center = width / 2, height * 0.8
-	
+
 	pygame.display.update()
 
-	counter = 0
-	
+
 	while running:
 		rock_loc[1] += 1
 		if rock_loc[1] > height:
@@ -43,32 +42,27 @@ def main():
 				rock_loc.center = third_col, -200
 			if coord == 3:
 				rock_loc.center = fourth_col, -200
-		
+
 		# end game
 		if rock_loc[0] == ship_loc[0] and rock_loc[1] > ship_loc[1] - 70:
 			print("KABOOM!")
 			break
-				
+
 		for event in pygame.event.get():
-		    if event.type == QUIT:
-		        running = False
-		    if event.type == KEYDOWN:
-		        if event.key in [K_a, K_LEFT]:
-		            ship_loc[0] -= 20
-		        if event.key in [K_d, K_RIGHT]:
-		            ship_loc[0] += 20
-		
+			if event.type == QUIT:
+				running = False
+			if keys[K_RIGHT]:
+				ship_loc += vel
+			if keys[K_LEFT]:
+				ship_loc -= vel
+
 		screen.blit(bg_img,(0,0))
 		screen.blit(ship, ship_loc)
 		screen.blit(rock, rock_loc)
 		pygame.display.update()
-	
+
 	pygame.quit()
-		
-		
+
+
 if __name__ == "__main__":
 	main()
-		
-
-
-
